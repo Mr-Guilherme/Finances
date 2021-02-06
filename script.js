33e7c032f92bda32e365e7449b374b3e033eeae9
Mr-Guilherme <guilherme.dev2019@gmail.com>
2021-02-06 18:39:32 -0400

    Remove static data of Html and create, start app with objects DOM, Transaction, Utils in script.js

--- script.js
+++ script.js
@@ -2,16 +2,167 @@ const buttonNew = document.querySelector("a.button.new")
 const buttonCancel = document.querySelector("a.button.cancel")
 const form = document.querySelector(".modal-overlay")
 
-const Modal = {
-    open(){
-        form.classList.add("active")
+buttonNew.addEventListener("click", toggle)
+buttonCancel.addEventListener("click", toggle)
+
+function toggle() {
+    form.classList.toggle("active")
+}
+
+const transactions = [
+    {
+        description: 'Site',
+        amount: 600000,
+        date: '25/01/2021'
     },
-    close(){
-        form.classList.remove("active")
+    {
+        description: 'Luz',
+        amount: -40000,
+        date: '10/01/2021'
+    },
+    {
+        description: 'Internet',
+        amount: -25000,
+        date: '02/01/2021'
+    },
+    {
+        description: 'Aluguel',
+        amount: -95000,
+        date: '01/01/2021'
+    },
+]
+
+const Transaction = {
+    all: transactions,
+
+    add(transaction) {
+        this.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index) {
+        this.all.splice(index, 1)
+
+        App.reload()
+    },
+
+    incomes(){
+        let income = 0
+
+        transactions.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount
+            }
+        })        
+
+        return income
+    },
+
+    expenses(){
+        let expense = 0
+
+        transactions.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            }
+        })        
+
+        return expense
+    },
+
+    total(){
+        return this.incomes() + this.expenses()
     }
 }
 
-buttonNew.addEventListener("click",Modal.open)
+const DOM = {
+    transactionContainer: document.querySelector('#data-table tbody'),
 
-buttonCancel.addEventListener("click", Modal.close)
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = this.innerHTMLTransaction(transaction)
+
+        this.transactionContainer.appendChild(tr)   
+    },
+
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img src="./assets/minus.svg" alt="Remover transação">
+        </td>`
+
+        return html
+    },
+
+    updateBalance() {
+        const incomeDisplay = document.querySelector('#incomeDisplay')
+
+        incomeDisplay.innerHTML = Utils.formatCurrency(Transaction.incomes())
+
+        const expenseDisplay = document.querySelector('#expenseDisplay')
+
+        expenseDisplay.innerHTML = Utils.formatCurrency(Transaction.expenses())
+
+        const totalDisplay = document.querySelector('#totalDisplay')
+
+        totalDisplay.innerHTML = Utils.formatCurrency(Transaction.total())
+
+    },
+
+    clearTransaction(){
+        this.transactionContainer.innerHTML = ""
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? '-' : ''
+
+        value = String(value).replace(/\D/g, '')
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+
+        return signal + value
+    }
+}
+
+const App = {
+    init() {
+     
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+
+    },
+    reload(){
+        DOM.clearTransaction()
+        
+        this.init()
+    }
+}
+
+App.init()
+
+Transaction.add({
+    description: 'App',
+    amount: 1000000,
+    date: '15/12/2020'
+})
+
+
 
